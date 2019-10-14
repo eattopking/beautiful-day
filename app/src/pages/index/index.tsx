4a75f06acbf1477d8ec9d3cb356e8be8521d733d
zhanghengh <zhanghengh@yonyou.com>
2019-10-14 22:19:05 +0800

    小程序发布成功

--- app/src/pages/index/index.tsx
+++ app/src/pages/index/index.tsx
@@ -82,7 +82,7 @@ class Index extends Component {
         <Button className='dec_btn' onClick={this.props.dec}>-</Button>
         <Button className='dec_btn' onClick={this.props.asyncAdd}>async</Button>
         <View><Text>{this.props.counter.num}</Text></View>
-        <View><Text>Hello, World</Text></View>
+        <View><Text>Hello, World111111111</Text></View>
       </View>
     )
   }
